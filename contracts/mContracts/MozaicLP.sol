539e19ab2478f637acbc3170d89db8d8a234549d
MachineLearningMike <mike@gmail.com>
2023-03-03 02:26:00 +0800

    mLP

--- contracts/mContracts/MozaicLP.sol
+++ contracts/mContracts/MozaicLP.sol
@@ -2,6 +2,36 @@
 
 pragma solidity ^0.8.0;
 
-contract MozaicLP {
-    
+import "../token/oft/composable/ComposableOFT.sol";
+
+/// @title A LayerZero OmnichainFungibleToken example of BasedOFT
+/// @notice Use this contract only on the BASE CHAIN. It locks tokens on source, on outgoing send(), and unlocks tokens when receiving from other chains.
+contract MozaicLP is ComposableOFT {
+    address public vault;
+
+    constructor(address _layerZeroEndpoint, uint _initialSupply) ComposableOFT("MozaicLiquidityProviderToken", "MLP", _layerZeroEndpoint) {
+        // No mint. _mint(_msgSender(), _initialSupply);
+    }
+
+    function setValut(address _vault) onlyOwner external virtual {
+        vault = _vault; // No need to require non-zero address. 
+    }
+
+    modifier onlyVault() {
+        require(vault == _msgSender(), "mLP: caller is not the vault");
+        _;
+    }
+
+    function _debitFrom(address _from, uint16, bytes memory, uint _amount) onlyVault  internal virtual override returns(uint) {
+        address spender = _msgSender();
+        if (_from != spender) _spendAllowance(_from, spender, _amount);
+        _burn(_from, _amount);
+        return _amount;
+    }
+
+    function _creditTo(uint16, address _toAddress, uint _amount) onlyVault internal virtual override returns(uint) {
+        _mint(_toAddress, _amount);
+        return _amount;
+    }
+
 }
---
+++ contracts/mContracts/MozaicVault.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/ILayerZeroReceiver.sol";
+import "../interfaces/ILayerZeroUserApplicationConfig.sol";
+import "../interfaces/ILayerZeroEndpoint.sol";
+import "../util/BytesLib.sol";
+
+contract MozaicVault {
+
+}
