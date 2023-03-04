9005bc291f20545f315eafc520056c6a929c8956
MachineLearningMike <mike@gmail.com>
2023-03-04 17:35:55 +0800

    LZ endpoint and node incorporated

--- deploy/ERC1155.js
+++ deploy/ERC1155.js
@@ -4,7 +4,7 @@ const ONFT_ARGS = require("../constants/onftArgs.json")
 module.exports = async function ({ deployments, getNamedAccounts }) {
     const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
-    console.log(`>>> your address: ${deployer}`)
+    console.log(`>>> your address!!!: ${deployer}`)
 
     const lzEndpointAddress = LZ_ENDPOINTS[hre.network.name]
     console.log(`[${hre.network.name}] Endpoint Address: ${lzEndpointAddress}`)
