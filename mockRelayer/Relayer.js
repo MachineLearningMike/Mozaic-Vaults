857fc6477d29d39c690e9f3f7df9d218533b2f7e
MachineLearningMike <mike@gmail.com>
2023-03-14 03:42:57 +0800

    mock relayer

--- mockRelayer/Relayer.js
+++ mockRelayer/Relayer.js
@@ -9,8 +9,33 @@ function consoleLogWithTab(str) {
     console.log(`\t${str}`);
 }
 
-const vaults = [];
-let owner, alice, bob, carol, dev;
-const nVaults = 5;
-let nMain = 0;
-let tx;
+
+const vaultAddresses = [];
+
+topicDeployed = ethers.utils.id("Deployed(address,address)");
+filterDeployed = {
+    topics: [topicDeployed],
+}
+
+function listenDeployed(log, event) {
+    if(log.topics.includes(topicDeployed)) {
+        console.log(log.topics);
+        vaultAddresses.push('0x' + log.topics[1].slice(26));
+    }
+}
+
+topicPacket = ethers.utils.id("Packet(bytes)");
+filterPacket = {
+    topics: [topicPacket],
+}
+
+
+function listenPacket(log, event) {
+    if(log.topics.includes(topicPacket)) {
+        console.log(log.data);
+    }
+}
+
+ethers.provider.removeAllListeners();
+ethers.provider.on(filterDeployed, listenDeployed);
+// ethers.provider.on(filterPacket, listenPacket);
